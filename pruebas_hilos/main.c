4bca79177b26c8917c785a5a21d1013375abd244
FranSagasti2609 <fransagasti2609@gmail.com>
2023-06-10 21:56:24 -0300

    Hilos

---
+++ pruebas_hilos/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h> //Libreria para trabajar con hilos
+#include <sys/types.h>
+
+void *holamundo(void *argumentos){
+    printf("Hola mundo, soy el primer hilo!.\n");
+}
+
+
+int main(){
+    
+    pthread_t hilo1;
+    //pthread_create(&hilo1, NULL, holamundo, NULL);
+    //pthread_join(hilo1, NULL);
+    printf("Fin\n");
+
+    return 0;
+}
